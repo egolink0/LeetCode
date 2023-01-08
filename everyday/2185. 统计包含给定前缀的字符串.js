fe3dd3abe4d31c6b93dd2bfc6214be774cfc5c6e
ego <18521972+running-little-snail@users.noreply.github.com>
2023-01-08 14:44:11 +0800

    Create 2185. 统计包含给定前缀的字符串.js

---
+++ everyday/2185. 统计包含给定前缀的字符串.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string[]} words
+ * @param {string} pref
+ * @return {number}
+ */
+// var prefixCount = function(words, pref) {
+//     let count = 0;
+//     words.forEach((w)=>{
+//         const p = w.slice(0,pref.length);
+//         if(p===pref){
+//             count++;
+//         }
+//     })
+//     return count;
+// };
+
+
+var prefixCount = function(words, pref) {
+    let count = 0;
+    words.forEach((w)=>{
+        if(w.startsWith(pref)){
+            count++;
+        }
+    })
+    return count;
+};
