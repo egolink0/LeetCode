a621928e0233df70ccbf4c76898ed14edc72754f
ego <18521972+egolink0@users.noreply.github.com>
2023-04-03 17:06:27 +0800

    Create 617. 合并二叉树.js

---
+++ everyday/617. 合并二叉树.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @return {TreeNode}
+ */
+var mergeTrees = function (root1, root2) {
+  if (!root1) return root2;
+  if (!root2) return root1;
+  const { left: left1, right: right1, val: val1 } = root1;
+  const { left: left2, right: right2, val: val2 } = root2;
+
+  root1.val = (val1 || 0) + (val2 || 0);
+  if (left1 && left2) root1.left = mergeTrees(left1, left2);
+  else if (left2 && !left1) root1.left = left2;
+
+  if (right1 && right2) root1.right = mergeTrees(right1, right2);
+  else if (right2 && !right1) root1.right = right2;
+
+  return root1;
+};
